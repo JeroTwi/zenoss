67b910aacd03032c2cc47b12de440f743daabf2c
JeroTwi <JeroTwi@users.noreply.github.com>
2018-12-31 14:21:14 +0100

    Update zo425_ubuntu-debian.sh

--- core-autodeploy/4.2.5/zo425_ubuntu-debian.sh
+++ core-autodeploy/4.2.5/zo425_ubuntu-debian.sh
@@ -9,7 +9,8 @@
 #      1 - Install Ubuntu 14.04
 #      2 - Update everything to the max
 #      3 - sudo apt-get install libpangocairo-1.0-0 libx11-xcb1 libxcomposite1 libxcursor1 libxdamage1 libxi6 libxtst6 libnss3 libcups2 libxss1 libxrandr2 libgconf2-4 libasound2 libatk1.0-0 libgtk-3-0
-#      4 - 
+#      4 - sudo apt-get install libxslt-dev
+#      5 - 
 ##########################################
 
 # Beginning Script Message
