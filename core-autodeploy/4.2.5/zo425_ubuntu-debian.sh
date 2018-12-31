23a42a322984609e1c88796d9cc33bf7e5ea67f0
JeroTwi <JeroTwi@users.noreply.github.com>
2018-12-31 14:28:55 +0100

    Update zo425_ubuntu-debian.sh

--- core-autodeploy/4.2.5/zo425_ubuntu-debian.sh
+++ core-autodeploy/4.2.5/zo425_ubuntu-debian.sh
@@ -11,7 +11,7 @@
 #      3 - sudo apt-get install libpangocairo-1.0-0 libx11-xcb1 libxcomposite1 libxcursor1 libxdamage1 libxi6 libxtst6 libnss3 libcups2 libxss1 libxrandr2 libgconf2-4 libasound2 libatk1.0-0 libgtk-3-0
 #      4 - sudo apt-get install libxslt-dev
 #      5 - sudo apt-get install snmp
-#      6 - 
+#      6 - sudo apt-get install build-essential
 ##########################################
 
 # Beginning Script Message
